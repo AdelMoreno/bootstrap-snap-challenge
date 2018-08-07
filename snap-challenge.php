6b1b572cb19411ef3e715bdbf75970b3d5ee0b24
Adel Moreno <adel.argenix@gmail.com>
2018-08-07 09:07:47 -0600

    worked on bootstrap snap challenge

---
+++ snap-challenge.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width-device-width, initial-scale=1, shrink-to-fit=no">
+
+		<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+		<link href="styles.css" rel="stylesheet" type="text/css">
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha256-98vAGjEDGN79TjHkYWVD4s87rvWkdWLHPs5MC3FvFX4=" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+		<title>Bootstrap Challenge</title>
+	</head>
+<body>
+	<header class="container border bg-primary rounded p-3 mb-2 text-dark mt-2 justify-content-center">
+		<div class="row">
+			<div class="col-md-2">
+				<img class="img-fluid rounded" src="https://images.cdn2.stockunlimited.net/preview1300/cat-and-dog-forming-a-heart_1305232.jpg"
+			</div>
+		</div>
+		<nav class="navbar navbar-expand-md navbar-light bg-primary">
+			<a class="navbar-brand" href="#"></a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item">
+						<a class="nav-link" href="#">Title</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Thoughts</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Friends</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Social</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	</header>
+	<main class="container text-center">
+		<div class="row">
+				<div class="card col-md-3">
+					<p class="card-header">Riker!</p>
+					<div class="card-body">
+						<p>aww yiss</p>
+					</div>
+				</div>
+			<div class="card col-md-3 border border-primary">
+				<p class="card-header">Ipsum!</p>
+				<div class="card-body">
+					<p>aww yiss</p>
+				</div>
+			</div>
+			<div class="card col-md-3 border border-danger">
+				<p class="card-header">Dot Dot Dot!</p>
+				<div class="card-body">
+					<p>aww yiss</p>
+				</div>
+			</div>
+			<div class="card col-md-3 border border-warning">
+				<p class="card-header">Heart!</p>
+				<div class="card-body">
+					<p>aww yiss</p>
+				</div>
+			</div>
+		</div>
+	</main>
+	<div class="container text-center">
+		<div class="row">
+			<div class="col-md">
+				<p>
+					Ruff pats length boy heckin angery woofer corgo sub woofer, doggorino corgo waggy wags. Wrinkler length boy much ruin diet ur givin me a spook blop puggo wow very biscit, pupperino very jealous pupper waggy wags ur givin me a spook vvv. Wow such tempt doggo many pats smol corgo h*ck long woofer borking doggo, pats doge big ol ur givin me a spook many pats heckin angery woofer. h*ck borkf. Fluffer sub woofer maximum borkdrive borking doggo he made many woofs wow such tempt, noodle horse puggorino sub woofer. Very good spot bork he made many woofs sub woofer heckin wrinkler doggorino many pats waggy wags aqua doggo, shoob woofer heckin good boys lotsa pats much ruin diet maximum borkdrive tungg woofer. Such treat woofer doing me a frighten very good spot, heckin good boys. Waggy wags ruff fluffer aqua doggo, pupper length boy.
+				</p>
+			</div>
+		</div>
+	</div>
+</body>
